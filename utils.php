bfde05fe292985e6dd75994b95fcb9404c0ccc85
François Kooman <fkooman@tuxed.net>
2013-04-22 22:34:51 +0200

    also close the curl session

--- utils.php
+++ utils.php
@@ -77,7 +77,9 @@ function getContents($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
 
-    return curl_exec($ch);
+    return $output;
 }
 ?>
